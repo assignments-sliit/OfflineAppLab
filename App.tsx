cbd84d8046397eb56492e77716f331aa92fc508a
Manoj Kumar <dharmakumar338@gmail.com>
2022-08-16 06:28:42 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,22 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { NetworkProvider, NetworkConsumer } from "react-native-offline";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <NetworkProvider shouldPing={true} pingInterval={100}>
+        <NetworkConsumer>
+          {({ isConnected }) =>
+            isConnected ? (
+              <Text style={styles.text}>
+                hello
+              </Text>
+            ) : (
+              <Text> No Network</Text>
+            )
+          }
+        </NetworkConsumer>
+      </NetworkProvider>
+    </View>
+  );
+};
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 100,
+    padding: 20,
+    borderColor: "black",
+    height: 55,
+    alignItems: "center",
+  },
+  text: {
+    flex: 1,
+    fontSize: 24,
+    color: "black",
+    textAlign: "center",
+  },
+});
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
-}
+export default App;
